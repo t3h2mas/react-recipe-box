17bd2f93ea54b87fe7f864320737b38b4eb13b3a
Thomas <jtruant@gmx.com>
2016-09-05 22:20:53 -0600

    localStorage persistance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import AppContainer from './containers/AppContainer';
 import reducer from './reducers/index';
 
-const store = createStore(reducer);
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
+
+const wrappedReducer = storage.reducer(reducer);
+const engine = createEngine('recipe-box');
+const middleware = storage.createMiddleware(engine);
+
+const createStoreWithMiddleware = applyMiddleware(middleware)(createStore);
+
+const store = createStoreWithMiddleware(wrappedReducer, window.devToolsExtension && window.devToolsExtension());
+
+const load = storage.createLoader(engine);
+load(store);
+
 
 const app = (
   <Provider store={store}>
